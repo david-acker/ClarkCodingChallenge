7b63752cb7d18c24ea1d5e5c1c2fca4b70139f79
DavidDAcker <davidacker3@gmail.com>
2021-03-23 12:45:47 -0400

    Created entity

---
+++ ClarkCodingChallenge/Entities/Contact.cs
@@ -0,0 +1,9 @@
+namespace ClarkCodingChallenge.Entities
+{
+    public class Contact
+    {
+        public string LastName { get; set; }
+        public string FirstName { get; set; }
+        public string Email { get; set; }
+    }
+}
